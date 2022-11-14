33cdc47a95a42159300e294b906a0e6adcf44e03
kompleeex <paaladam1@gmail.com>
2022-11-14 13:07:33 +0100

    projekt

---
+++ src/oopdolgozat_1/OOPdolgozat_1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oopdolgozat_1;
+
+/**
+ *
+ * @author paal.adam
+ */
+public class OOPdolgozat_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
